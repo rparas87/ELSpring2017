efd961e80a1b5c1677caf78fff86aeaa55e8ca67
rparas87 <rparas87@gmail.com>
2017-03-07 22:13:03 -0500

    Add files via upload

---
+++ code/myBlinkingLed.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17,GPIO.OUT)
+
+def Blink():
+  x = 0
+  while x == 0:
+    for i in range(0,3):
+      print "blink #" + str(i+1)
+      GPIO.output(17,True)
+      time.sleep(.2)
+      GPIO.output(17,False)
+      time.sleep(.1)
+    time.sleep(5)
+    for i in range(0,4):
+      print "blink #" + str(i+1)
+      GPIO.output(17,True)
+      time.sleep(.2)
+      GPIO.output(17,False)
+      time.sleep(.1)
+    time.sleep(1)
+    print "done!!"
+  GPIO.cleanup()
+Blink()
